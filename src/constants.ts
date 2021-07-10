992ff69040ad5b6cb1e5c4688586045b14d2a38e
Luke Leppan <lukelepp@gmail.com>
2021-07-10 20:43:45 +0200

    🔀 Merge some stuff

--- src/constants.ts
+++ src/constants.ts
@@ -9,4 +9,4 @@ export const MATCH_HTML_COMMENT = new RegExp(
     "|<[?][^>]*>?",
   "g"
 );
-export const MATCH_COMMENT = new RegExp("(%%(w+(sw+)*)%%)", "g");
+export const MATCH_PARAGRAPH = new RegExp("\n([^\n]+)\n", "g");
