9885035ac8e73e10204a94527502e8b608f99831
minermaniac447 <42500591+minermaniac447@users.noreply.github.com>
2023-02-08 16:12:36 -0500

    Add Pages

--- src/stats/Stats.ts
+++ src/stats/Stats.ts
@@ -9,10 +9,12 @@ export interface Day {
   words: number;
   characters: number;
   sentences: number;
+  pages: number;
   files: number;
   totalWords: number;
   totalCharacters: number;
   totalSentences: number;
+  totalPages: number;
 }
 
 export type ModifiedFiles = Record<string, FileStat>;
@@ -21,6 +23,7 @@ export interface FileStat {
   words: CountDiff;
   characters: CountDiff;
   sentences: CountDiff;
+  pages: CountDiff;
 }
 
 export interface CountDiff {
